8c425f99f341b7257081871ee9349c285176c248
Frank <frankborden2@gmail.com>
2019-04-13 20:49:57 -0700

    websocket test

--- bin/server.dart
+++ bin/server.dart
@@ -1,8 +1,20 @@
 import 'package:appengine/appengine.dart';
-import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf/shelf_io.dart';
 import 'package:shelf_static/shelf_static.dart';
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 main() async {
-  await runAppEngine((request) => io.handleRequest(
-      request, createStaticHandler('./build', defaultDocument: 'index.html')));
+  await runAppEngine((request) {
+    if (request.uri.path == '/test') {
+      handleRequest(request, webSocketHandler((WebSocketChannel webSocket) {
+        webSocket.stream.listen((message) {
+          webSocket.sink.add("echo $message");
+        });
+      }));
+    } else {
+      handleRequest(request,
+          createStaticHandler('./build', defaultDocument: 'index.html'));
+    }
+  });
 }
--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,17 @@
+import 'dart:html';
+
 import 'package:puyo/src/core/model/state.dart';
 import 'package:puyo/src/shell/runner.dart';
+import 'package:web_socket_channel/html.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 main() {
+  WebSocketChannel channel =
+      HtmlWebSocketChannel.connect('ws://${window.location.host}/test');
+  channel.stream.listen((message) {
+    print(message);
+  });
+  channel.sink.add('hello websocket');
+
   Runner().run(initialState);
 }
